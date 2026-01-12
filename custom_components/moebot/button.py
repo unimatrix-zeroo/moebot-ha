9051810c5726d1a1ead978d5cf5b5b41eec762a6
unimatrix-zeroo <69356938+unimatrix-zeroo@users.noreply.github.com>
2026-01-12 07:55:07 +0100

    Update button.py

--- custom_components/moebot/button.py
+++ custom_components/moebot/button.py
@@ -1,30 +1,35 @@
-from homeassistant.components.button import ButtonEntity, ButtonDeviceClass
+from __future__ import annotations
+
+from homeassistant.components.button import (
+    ButtonEntity,
+    ButtonDeviceClass,
+)
 from homeassistant.helpers.entity import EntityCategory
+
+from pymoebot import MoeBot
 
 from . import BaseMoeBotEntity
 from .const import DOMAIN
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Add sensors for passed config_entry in HA."""
-    moebot = hass.data[DOMAIN][config_entry.entry_id]
-
+    """Set up MoeBot button entities."""
+    moebot: MoeBot = hass.data[DOMAIN][config_entry.entry_id]
     async_add_entities([PollDeviceButton(moebot)])
 
 
 class PollDeviceButton(BaseMoeBotEntity, ButtonEntity):
+    """Button to manually poll the MoeBot device."""
 
-    def __init__(self, moebot):
+    _attr_has_entity_name = True
+    _attr_name = "Poll Device"
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_device_class = ButtonDeviceClass.UPDATE
+
+    def __init__(self, moebot: MoeBot) -> None:
         super().__init__(moebot)
+        self._attr_unique_id = f"{moebot.id}_poll_device"
 
-        # A unique_id for this entity within this domain.
-        # Note: This is NOT used to generate the user visible Entity ID used in automations.
-        self._attr_unique_id = f"{self._moebot.id}_poll_device"
-
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
-
-        self._attr_device_class = ButtonDeviceClass.UPDATE
-        self._attr_name = "Poll Device"
-
-    def press(self) -> None:
-        self._moebot.poll()
+    async def async_press(self) -> None:
+        """Handle the button press."""
+        await self.hass.async_add_executor_job(self._moebot.poll)
