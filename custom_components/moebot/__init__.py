76c6485a145364987e5883abb10c8a999b2d2c35
unimatrix-zeroo <69356938+unimatrix-zeroo@users.noreply.github.com>
2026-01-12 07:47:52 +0100

    Update __init__.py

--- custom_components/moebot/__init__.py
+++ custom_components/moebot/__init__.py
@@ -5,31 +5,48 @@ import logging
 from datetime import datetime
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform, EVENT_HOMEASSISTANT_STOP
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity, DeviceInfo
+
 from pymoebot import MoeBot
 
 from .const import DOMAIN
 
-PLATFORMS: list[Platform] = [Platform.VACUUM, Platform.SENSOR, Platform.NUMBER, Platform.SWITCH, Platform.BUTTON,
-                             Platform.LAWN_MOWER]
-_log = logging.getLogger(__package__)
+PLATFORMS: list[Platform] = [
+    Platform.VACUUM,
+    Platform.SENSOR,
+    Platform.NUMBER,
+    Platform.SWITCH,
+    Platform.BUTTON,
+    Platform.LAWN_MOWER,
+]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up MoeBot from a config entry."""
-    moebot = await hass.async_add_executor_job(MoeBot, entry.data["device_id"], entry.data["ip_address"],
-                                               entry.data["local_key"])
-    _log.info("Created a moebot: %r" % moebot)
+
+    moebot = await hass.async_add_executor_job(
+        MoeBot,
+        entry.data["device_id"],
+        entry.data["ip_address"],
+        entry.data["local_key"],
+    )
+
+    _LOGGER.info("Created MoeBot device %s", moebot.id)
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = moebot
+
     await hass.async_add_executor_job(moebot.listen)
 
-    def shutdown_moebot(event):
-        _log.debug("In the shutdown callback")
+    def _shutdown() -> None:
+        _LOGGER.debug("Stopping MoeBot listener")
         moebot.unlisten()
 
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, shutdown_moebot)
+    # Ensure clean shutdown on unload OR HA stop
+    entry.async_on_unload(_shutdown)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
@@ -37,47 +54,50 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        moebot = hass.data[DOMAIN][entry.entry_id]
+        moebot: MoeBot = hass.data[DOMAIN].pop(entry.entry_id)
         moebot.unlisten()
-        hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
 
 
 class BaseMoeBotEntity(Entity):
-    """The abstract base device for all MoeBot entities."""
+    """Base entity for all MoeBot entities."""
 
-    def __init__(self, moebot: MoeBot):
+    _attr_should_poll = False
+
+    def __init__(self, moebot: MoeBot) -> None:
         self._moebot = moebot
 
-        # MoeBot class is LOCAL PUSH, so we tell HA that it should not be polled
-        self._attr_should_poll = False
-
-        # Link this Entity under the MoeBot (vacuum) device by ensuring this property returns an
-        # identifiers value matching that used in the vacuum Entity, but no other information.
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._moebot.id)}
+            identifiers={(DOMAIN, moebot.id)}
         )
-
-    @property
-    def extra_state_attributes(self):
-        if self._moebot.last_update is not None:
-            return {"last_message_received": datetime.fromtimestamp(self._moebot.last_update)}
-        return None
-
-    async def async_added_to_hass(self) -> None:
-        """Run when this Entity has been added to HA."""
-
-        # The call back registration is done once this entity is registered with HA
-        # (rather than in the __init__)
-        def listener(raw_msg):
-            _log.debug("%r got an update: %r" % (self.__class__.__name__, raw_msg))
-            # self.async_write_ha_state()
-            self.schedule_update_ha_state()
-
-        self._moebot.add_listener(listener)
 
     @property
     def available(self) -> bool:
         return self._moebot.online
+
+    @property
+    def extra_state_attributes(self) -> dict | None:
+        if self._moebot.last_update is not None:
+            return {
+                "last_message_received": datetime.fromtimestamp(
+                    self._moebot.last_update
+                )
+            }
+        return None
+
+    async def async_added_to_hass(self) -> None:
+        """Register MoeBot push listener."""
+
+        def _listener(raw_msg: dict) -> None:
+            _LOGGER.debug(
+                "%s received update: %s",
+                self.__class__.__name__,
+                raw_msg,
+            )
+            # Thread-safe state update
+            self.hass.loop.call_soon_threadsafe(self.async_write_ha_state)
+
+        self._moebot.add_listener(_listener)
